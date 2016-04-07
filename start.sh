a043e2282f5a4bed8f7e004055eef2135bf80abb
km_digitalpatrioten <kevin@digitalpatrioten.com>
2016-04-07 16:39:24 +0200

    Remove xdebug switch for cli

--- start.sh
+++ start.sh
@@ -28,6 +28,7 @@ echo -e 'mailhub='$SMTP_HOST'\nAuthUser='$SMTP_USER'\nAuthPass='$SMTP_PASSWORD'\
 
 if [ ! -z "$DISABLE_XDEBUG" ]; then
     rm -rf /etc/php/7.0/fpm/conf.d/20-xdebug.ini
+    rm -rf /etc/php/7.0/cli/conf.d/20-xdebug.ini
 fi
 
 chmod 0600 /var/spool/cron/crontabs/*
